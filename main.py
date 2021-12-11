92827339ac911f34bd8103ffa86c7b94f9c4e1d9
SloMoJo <65332660+SloMoJo@users.noreply.github.com>
2021-12-11 18:31:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 import pygame
-import sys
-import numpy as np
+import config
+from game import Game
+from game_state import GameState
 
 #general setup
 pygame.init()
-clock = pygame.time.Clock()
 
 #display surface
-screen = pygame.display.set_mode((800,800))
-second_surface = pygame.Surface([100,200])
-second_surface.fill((0,0,255))
+screen = pygame.display.set_mode((600,400))
+pygame.display.set_caption("3 FAZ The Game")
 
-ophelia = pygame.image.load('cover_art_ophelia.png')
-ophelia_rect = ophelia.get_rect(topleft = [100,200])
+clock = pygame.time.Clock()
+game = Game(screen)
+game.set_up()
 
+while game.game_state == GameState.RUNNING:
+    clock.tick(50)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    screen.fill((255,255,255))
-    screen.blit(second_surface,(0,50))
-    screen.blit(ophelia,ophelia_rect)
-    ophelia_rect.right += 5
-
+    game.update()
     pygame.display.flip()
-    clock.tick(60)
